1a1a33223a82038b759f378e35b509ecb068a1fa
ISIGE, Akira <xia@silvia.com>
2019-02-19 15:56:19 +0900

    末尾が / の時に階層を上がるのに失敗気味なのを修正

--- js/content.js
+++ js/content.js
@@ -1,17 +1,19 @@
 if (location.search){
-    // Cut query string
-	location.search = '';
+  // Cut query string
+  location.search = '';
 } else if (location.pathname == '/'){
-	var domains = location.hostname.split('.');
+  var domains = location.hostname.split('.');
 
-	if (domains.length > 1){
-		// Cut subdomain
-		domains.shift();
-		location.hostname = domains.join('.');
-	}
+  if (domains.length > 1){
+    // Cut subdomain
+    domains.shift();
+    location.hostname = domains.join('.');
+  }
 } else {
-	// Cut path
-	var comps = location.pathname.split('/');
-	comps.pop();
-	location.pathname = comps.join('/');
+  // Cut path
+  var comps = location.pathname.split('/');
+  if (comps.pop() === '') {
+    comps.pop();
+  }
+  location.pathname = comps.join('/') + '/';
 }
